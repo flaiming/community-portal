92d0995cbd483a2228c20d2edb150706f6ced92a
Vojtech Oram <voram@redhat.com>
2016-03-18 15:53:22 +0100

    Initial commit

---
+++ portal/urls.py
@@ -0,0 +1,31 @@
+"""portal URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from machina.app import board
+
+from views import index, UserSettingsView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/profile/', UserSettingsView.as_view(), name='account-profile'),
+    url(r'^accounts/', include('allauth.urls')),
+
+    url(r'^markdown/', include('django_markdown.urls')),
+    url(r'^forum/', include(board.urls)),
+    url(r'^$', index, name='base-index')
+]
---
+++ portal/views.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, FormView
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse_lazy
+
+
+def index(request):
+    return render(request, "base.html", {})
+
+
+class UserSettingsView(UpdateView):
+    template_name = "user.html"
+    model = User
+    #form_class = UserSettingsForm
+    fields = ["username", "first_name", "last_name"]
+    success_url = reverse_lazy("account-profile")
+
+    #def form_valid(self, form):
+    #    instance = form.save(commit=False)
+    #    if 'icon' in form.files:
+    #        try:
+    #            userdata = instance.userdata
+    #            userdata.icon = form.files['icon']
+    #            userdata.save()
+    #        except UserData.DoesNotExist:
+    #            userdata = UserData(user=instance, icon=form.files['icon'])
+    #            userdata.save()
+    #            instance.userdata = userdata
+    #    messages.success(self.request, _("Settings successfully updated."))
+    #    return super(UserSettingsView, self).form_valid(form)
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
